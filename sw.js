9e03050f0909511c67ea107bf1a037453ad6ab9e
icerdenb-creator <icerdenb@gmail.com>
2026-08-08 17:33:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,50 @@
-const CACHE = "sac-bakim-pwa-v1";
-const ASSETS = ["./", "./sac_bakim_cizelgesi.html", "./manifest.webmanifest"];
+const CACHE = "sac-bakim-pwa-v2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
 
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
     )
   );
+
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(cached =>
-      cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match("./sac_bakim_cizelgesi.html"))
-    )
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+
+          caches.open(CACHE).then(cache => {
+            cache.put(event.request, copy);
+          });
+
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
   );
 });
